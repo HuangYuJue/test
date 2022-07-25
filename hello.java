c1338aa7f53a3657882de4ccaec1ac4a070e6f2b
HuangYuJue <2724074124@qq.com>
2022-07-25 22:02:05 +0800

    修改输出

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-public class hello {
+ublic class hello {
     public static void main(String[] args) {
         System.out.println("请输入名字：");
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
-        System.out.println(line+"欢迎您~");
+        System.out.println(line+"欢迎您de访问~");
     }
 }
